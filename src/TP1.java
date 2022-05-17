56a386cfe2fb3ed4241f6f065a42bbacb8fdeaad
Victor <victor.blangy@gmail.com>
2022-05-17 10:37:35 +0200

    tp1

---
+++ src/TP1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TP1 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int diameter;
+        float rayon;
+        double surface;
+
+        System.out.print("Saisir un diamètre : ");
+        diameter = scanner.nextInt();
+
+        rayon = diameter/2;
+        surface = (rayon * rayon) * Math.PI;
+
+        System.out.print("surface : " + surface);
+    }
+}
